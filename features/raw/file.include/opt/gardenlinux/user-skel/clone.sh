3dacb8bdca7af9ea5a75bd96832ceb75a8485d12
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-01-04 16:09:54 +0100

    Revert "cleanup"
    This reverts commit 8d2f426c3ff5cec80b0739c95dc6dc5aa7f08830.

--- features/raw/file.include/opt/gardenlinux/user-skel/clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-gh repo clone gardenlinux/gardenlinux
-gh repo clone gardenlinux/builder
-gh repo clone gardenlinux/builder_example
--- features/raw/file.include/opt/gardenlinux/user-skel/make-dev-ssh-key.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-ssh-keygen -b 1024 -t rsa -f ~/.ssh/id_rsa -q -N ""
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
