cdd23390de80fbddee2e7c4700aa49644a1da3ad
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-01-10 12:48:11 +0000

    shellcheck

--- features/raw/file.include/opt/gardenlinux/user-setup.sh
+++ features/raw/file.include/opt/gardenlinux/user-setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-set -x
-
 CONFIG=$(sudo cat /sys/firmware/qemu_fw_cfg/by_name/gardenlinux-dev-image-user-config/raw)
 
-if [[ ! -z CONFIG ]]; then
-    echo Installing user-config from $CONFIG
-    sudo --user=dev git clone https://github.com/$CONFIG /home/dev/user-config
+if [[ -n $CONFIG ]]; then
+    echo Installing user-config from "$CONFIG"
+    sudo --user=dev git clone https://github.com/"$CONFIG" /home/dev/user-config
     if [[ -f /home/dev/user-config/config.sh ]]; then
         if [[ -x /home/dev/user-config/config.sh ]]; then
             sudo --user=dev /home/dev/user-config/config.sh
