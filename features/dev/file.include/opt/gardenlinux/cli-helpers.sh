116a6c8b7025c9d99ab46ceb67c269d5e1488787
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-01-24 14:17:44 +0100

    uefi-vars

--- features/dev/file.include/opt/gardenlinux/cli-helpers.sh
+++ features/dev/file.include/opt/gardenlinux/cli-helpers.sh
@@ -4,6 +4,11 @@ if [ -f /usr/lib/git-core/git-sh-prompt ]; then
 	export PS1='\w$(__git_ps1 " (%s)")\$ '
 fi
 
+# As described here https://github.com/gardenlinux/gardenlinux/blob/169f15c70afbae7b578839520eeedb9ed2619c02/docs/02_operators/deployment/aws-secureboot.md?plain=1#L107
+if [ -d /opt/gardenlinux/python-uefivars ]; then
+	export PATH=/opt/gardenlinux/python-uefivars:$PATH
+fi
+
 alias ls='ls --group-directories-first --time-style=long-iso --color=auto --classify'
 alias ll='ls -lh'
 alias la='ls -lah'
